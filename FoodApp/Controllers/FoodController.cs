bb78eee87c3ce31277108b989fb239126a89f197
xhenimane10 <167853338+xhenimane10@users.noreply.github.com>
2024-05-02 13:34:21 +0200

    Update FoodController.cs

--- FoodApp/Controllers/FoodController.cs
+++ FoodApp/Controllers/FoodController.cs
@@ -1,34 +1,69 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using FoodApp.Dto;
 using FoodApp.Services;
 
 namespace FoodApp.Controllers
 {
-[Route("api/[controller]")]
-[ApiController]
-public class FoodController : ControllerBase
-{
-    [HttpPut("Name")]
-    public IActionResult GetNameByName(string name)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
     {
-        return Ok();
-    }
 
-    [HttpPost("Calories")]
-    public IActionResult GetCalByCal(float cal)
-    {
-        return Ok();
+        [HttpGet("All")]
+        public IActionResult GetFood()
+        {
+            //List of students
+            var allFood = new List<FoodApp.models.Food>()
+            {
+                  new FoodApp.models.Food()
+                  {
+                    Gr = 1,
+                    Name = "First Food",
+                  
+                  },
+                   new FoodApp.models.Food()
+                   {
+                    Gr = 2,
+                    Name = "Second Food",
+           
+                   },
+            };
+
+            return Ok(allFood);
+        }
+
+        [HttpGet]
+        public IActionResult GetFoodAppByCal(float cal)
+        {
+            var newFoodApp = new models.Food()
+            {
+                Gr = 1,
+                Name = "First  FoodApp",
+            };
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddNewFoodApp([FromBody] PostFoodDto payload)
+        {
+            return Ok(payload);
+        }
+
+
+        [HttpPut("Name")]
+        public IActionResult UpdatFoodApp([FromBody] PutFoodDto payload, string name)
+        {
+            return Ok(payload);
+        }
+
+
+        [HttpDelete]
+        public IActionResult DeleteFoodApp(int gr)
+        {
+            return Ok();
+        }
     }
-    [HttpOptions("Gram")]
-    public IActionResult GetGrbyGr(int gr)
-    {
-        return Ok();
-    }
-    [HttpDelete("Name")]
-    public IActionResult GetNamebyName(string name)
-    {
-        return Ok();
-    }
-}
 }
