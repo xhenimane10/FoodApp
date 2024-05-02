769d2b53309aa96a40b003b25c034e26fa355786
xhenimane10 <167853338+xhenimane10@users.noreply.github.com>
2024-05-02 13:36:28 +0200

    Update SqlFoodAppService.cs

--- FoodApp/Services/SqlFoodAppService.cs
+++ FoodApp/Services/SqlFoodAppService.cs
@@ -1,23 +1,34 @@
-﻿using FoodApp.Dto;
+﻿using FoodApp.models;
 using System.Reflection;
 namespace FoodApp.Services
 {
-    public class SqlFoodAppService: IFoodAppService
+    public class SqlFoodAppService : IFoodAppService
     {
         public models.Food AddFoodApp(PostFoodDto foodapp)
         {
             throw new NotImplementedException();
         }
-        public void DeleteFoodApp(string name)
+        public void DeleteFoodApp(int gr)
         {
             throw new NotImplementedException();
         }
+
+        public List<Food> GetAllFood()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<models.Food> GetAllFoodApp()
+        {
+            throw new NotImplementedException();
+        }
+
         public models.Food GetFoodAppByName(string name)
         {
             throw new NotImplementedException();
         }
 
-        public models.Food UpdateFoodApp(PutFoodDto fooddata,string name)
+        public models.Food UpdateFoodApp(PutFoodDto fooddata, string name)
         {
             throw new NotImplementedException();
         }
